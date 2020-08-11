2dc47fb9ef45286016af253abb145884a848efa4
LHLLZSYLSC131456 <1156421151@qq.com>
2020-08-11 17:07:46 +0800

    测试数据

---
+++ Spring-Boot-1/src/main/java/com/demo/service/impl/CeShiServiceImpl.java
@@ -0,0 +1,14 @@
+package com.demo.service.impl;
+
+import com.demo.service.CeShiService;
+
+public class CeShiServiceImpl implements CeShiService {
+
+	@Override
+	public String findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
